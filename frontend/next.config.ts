f62cd3fad196c471b671c64a91a7a309ca963502
Priyanshu Saini <sainipriyanshu324@gmail.com>
2026-02-25 19:48:11 +0530

    Remove TypeScript ignoreBuildErrors option
    Removed TypeScript build error ignoring in config.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  typescript:{
-    ignoreBulidErors: true
-  },
   
   /* config options here */
   reactCompiler: true,
