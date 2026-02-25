bbb0e76449a1d39efef67cc50854755e86a69368
priyanshu324 <sainipriyanshu324@gmail.com>
2026-02-25 22:17:55 +0530

    update the app.js backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://192.168.1.10:3000"],
+    origin: [
+      "https://yt-features.vercel.app",
+      "https://yt-features-82azmh9wz-priyanshu845s-projects.vercel.app",
+    ],
     credentials: true,
   }),
 );
